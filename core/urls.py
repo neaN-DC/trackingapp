dc0ebad41e558e336de3fd83a9f300fe791085d8
neaN-DC <65737740+neaN-DC@users.noreply.github.com>
2020-12-12 01:21:01 +0000

    update

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from .views import save_player_id
 
 urlpatterns = [
 	path('', views.home, name = 'home'),
-	path('saveid/', views.save_player_id, name = 'home'),
+	path('saveid/', views.save_player_id),
 
 ]
 
